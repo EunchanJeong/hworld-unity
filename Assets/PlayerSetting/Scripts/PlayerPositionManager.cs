f5af979058ee4c2692d2f78ec4449ff0ff23ad00
YeongWook <ehdrnr16whduddnr@naver.com>
2024-09-23 01:06:37 +0900

    :bulb: Comment: 주석 작성

--- Assets/PlayerSetting/Scripts/PlayerPositionManager.cs
+++ Assets/PlayerSetting/Scripts/PlayerPositionManager.cs
@@ -1,5 +1,14 @@
 using UnityEngine;
 
+/// <summary>
+/// 캐릭터의 위치를 저장하고 불러오기 위한 스크립트
+/// </summary>
+/// <author>조영욱</author>
+/// <since>2024.09.09</since>
+/// <version>1.0</version>
+/// <remarks>
+/// 수정일: 2024.09.09, 수정자: 조영욱, 최초 생성
+/// </remarks>
 public class PlayerPositionManager : MonoBehaviour
 {
     public static Vector3 savedPosition;
